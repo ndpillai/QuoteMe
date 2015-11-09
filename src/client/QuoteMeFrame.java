0a057403c74e8f44f8013d37eba984b2bc56d7a6
Laura Gouillon <lgouillon@yahoo.com>
2015-11-08 16:22:58 -0800

    QuoteMeFrame getting started, card layout

--- src/client/QuoteMeFrame.java
+++ src/client/QuoteMeFrame.java
@@ -1,13 +1,65 @@
 package client;
 
+import java.awt.CardLayout;
+import java.awt.Container;
+
 import javax.swing.JFrame;
+
+/*
+ * 
+ * Member variables added:
+ * CardLayout layout
+ * Container pane
+ * 
+	Methods added:
+	initializeComponents
+	create GUI
+	addEvents
+	main
+*/
+
 
 public class QuoteMeFrame extends JFrame {
 	
 	private DataManager dataManager;
+	private CardLayout layout;
+	private Container pane;
 	
-	public QuoteMeFrame()
-	{
+	private HomePageGUI homePagePanel;
+	
+	public QuoteMeFrame() {
+		super("QuoteMe");
+		initializeComponents();
+		createGUI();
+		addEvents();
+		displayTab("Main Menu");
+	}
+	
+	private void initializeComponents() {
+		layout = new CardLayout();
+		pane = getContentPane();
+		pane.setLayout(layout);
 		
+		homePagePanel = new HomePageGUI();
+		//homePagePanel = dataManager.getHomePageGUI();
+	}
+	
+	private void createGUI() {
+		setSize(500, 750);
+		pane.add(homePagePanel, "Home Page");
+		displayTab("Home Page");
+	}
+	
+	private void addEvents() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	private void displayTab(String name) {
+		layout.show(this.getContentPane(), name);
+	}
+	
+	public static void main(String [] args) {
+		QuoteMeFrame qmf = new QuoteMeFrame();
+		qmf.setVisible(true);
 	}
 }
