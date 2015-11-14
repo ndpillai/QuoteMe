abf030e785645d8527606317ac4449677b5da8eb
Laura Gouillon <lgouillon@yahoo.com>
2015-11-14 15:32:47 -0800

    Linked different buttons in main panel to respective panels

--- src/client/MainPanel.java
+++ src/client/MainPanel.java
@@ -1,20 +1,29 @@
 package client;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 
 public class MainPanel extends JPanel {
 	
-	private JTabbedPane menuTabs;
 	private ApproveQuoteGUI approveQuotePanel;
-	public FeedPageGUI feed;
-	public PostQuoteGUI postQuote;
-	public ProfilePageGUI profilePage;
-	public NotificationGUI notifications;
+	private FeedPageGUI feed;
+	private PostQuoteGUI postQuote;
+	private ProfilePageGUI profilePage;
+	private NotificationGUI notifications;
 	private QuoteGUI quotePanel;
 	private WriteQuotePanel writeQuotePanel;
 	
-	private ClientPanel clientPanel;
+	private JTextField searchField;
+	private JButton notificationButton, writeQuoteButton, profilePageButton, feedPageButton;
+	
+	public ClientPanel clientPanel;
 
 	public User currentuser;
 	
@@ -26,21 +35,86 @@ public class MainPanel extends JPanel {
 	}
 	
 	private void initializeVariables() {
-
+		approveQuotePanel = new ApproveQuoteGUI(this);
+		feed = new FeedPageGUI(this);
+		postQuote = new PostQuoteGUI(this);
+		profilePage = new ProfilePageGUI(this);
+		notifications = new NotificationGUI(this);
+		quotePanel = new QuoteGUI(this);
+		writeQuotePanel = new WriteQuotePanel(this);
+		
+		notificationButton = new JButton("Notifications");
+		writeQuoteButton = new JButton("Write Quote");
+		profilePageButton = new JButton("My Profile");
+		feedPageButton = new JButton("Feed");
 	}
 	
-	private void createGUI()
-	{
+	private void createGUI() {
+		setLayout(new BorderLayout());
+		
+		// NORTH Panel
+		// will need an image for our logo, and a textfield for searching?
+		JLabel northLabel = new JLabel("We need to add a logo here");
+		searchField = new JTextField();
+		JPanel northPanel = new JPanel();
+		northPanel.add(northLabel);
+		northPanel.add(searchField);
+		add(northPanel, BorderLayout.NORTH);
+		
+		// SOUTH Panel
+		JPanel southPanel = new JPanel(new GridLayout(4,1));
+		southPanel.add(feedPageButton);
+		southPanel.add(writeQuoteButton);
+		southPanel.add(notificationButton);
+		southPanel.add(profilePageButton);
+		add(southPanel, BorderLayout.SOUTH);
+	}
+	
+	private void addEvents() {
+		feedPageButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				displayFeedPage();
+			}
+		});
+		writeQuoteButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				displayWriteQuotePage();			
+			}
+		});
+		notificationButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				displayNotificationPage();
+			}
+		});
+		profilePageButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				//displayProfilePage();
+			}
+		});
+	}
+	
+	private void refreshComponents() {
+		System.out.println("Refreshed components in main panel");
+	}
+	
+	public void displayFeedPage() {
+		//refreshComponents();
+		add(feed, BorderLayout.CENTER);
+	}
+	
+	public void displayWriteQuotePage() {
 		
 	}
 	
-	private void addEvents()
-	{
+	public void displayNotificationPage() {
 		
 	}
 	
-	public void main(String[] args)
-	{
+	public void displayProfilePage(User user) {
 		
 	}
 }
