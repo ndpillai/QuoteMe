11828793195107aca0c08e54cd4e6d1a29525b90
Laura Gouillon <lgouillon@yahoo.com>
2015-11-14 13:59:04 -0800

    QuoteMeClient updated with QuoteMeFrame instantiation and Socket on 6789

--- src/client/QuoteMeClient.java
+++ src/client/QuoteMeClient.java
@@ -1,14 +1,22 @@
 package client;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class QuoteMeClient {
 
-	public QuoteMeClient()
-	{
-		
+	public QuoteMeClient() {
+		QuoteMeFrame qmf = new QuoteMeFrame();
+		qmf.setVisible(true);
+		try {
+			Socket s = new Socket("localhost", 6789);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	public static void main(String[] args)
-	{
-		
+	public static void main(String[] args) {
+		new QuoteMeClient();
 	}
 }
