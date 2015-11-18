4f144d56916f0eb74c4de59fddf660a1c95db5f2
Tony Elevathingal <elevathi@usc.edu>
2015-11-18 02:46:33 -0800

    Added 2 functions to DataManager code that illustrate how to: 1. Modify the Data Manager and save those changes in the text file. 2. Return the Data Manager that's in the text file.

--- src/client/DataManager.java
+++ src/client/DataManager.java
@@ -1,5 +1,12 @@
 package client;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Vector;
 
@@ -50,4 +57,47 @@ public class DataManager {
 		return posterToQuoteMap;
 	}
 	
+	public static void updateDataManagerTextFile() {		
+		try {
+			// pull table model
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream("DataManager.txt"));
+			DataManager dataManager = (DataManager)ois.readObject();
+			ois.close();
+			
+			// modify Data Manager somehow
+//			**************
+			
+			// push modified Data Manager
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("DataManager.txt"));
+			oos.writeObject(dataManager);
+			oos.flush();
+			oos.close();
+		} catch (FileNotFoundException fnfe) { System.out.println("FileNotFoundException: " + fnfe.getMessage()); } catch (IOException ioe) { System.out.println("IOException: " + ioe.getMessage()); } catch (ClassNotFoundException cnfe) { System.out.println("ClassNotFoundException: " + cnfe.getMessage()); }
+	}
+	
+	public static DataManager readDataManagerFromTextFile() {
+		DataManager dataManager = new DataManager();
+		
+		try {
+			File file = new File("DataManager.txt");
+			
+			if(!file.exists()) { // if file doesn't exist, create it and push an empty Data Manager to it
+				file.createNewFile();
+				
+				DataManager newDataManager = new DataManager();
+				
+				ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("DataManager.txt"));
+				oos.writeObject(newDataManager);
+				oos.flush();
+				oos.close();
+			}
+			
+			// read in the Data Manager
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream("DataManager.txt"));
+			dataManager = (DataManager)ois.readObject();
+			ois.close();
+		} catch (FileNotFoundException fnfe) { System.out.println("FileNotFoundException: " + fnfe.getMessage()); } catch (IOException ioe) { System.out.println("IOException: " + ioe.getMessage()); } catch (ClassNotFoundException cnfe) { System.out.println("ClassNotFoundException: " + cnfe.getMessage()); }
+		
+		return dataManager;
+	}
 }
