e2f9042cda7bedfa0435f68bccab95e3e07e437c
Ingrid Wang <ingridwa@>
2015-11-21 18:57:27 -0800

    Updated Constants file to include frame width and height

--- src/resources/Constants.java
+++ src/resources/Constants.java
@@ -1,5 +1,8 @@
 package resources;
 
 public class Constants {
-	public static final String exampleConstant = "WOOHOO";
+	public static final int FrameWidth = 450;
+	public static final int FrameHeight = 700;
+	
+	
 }
