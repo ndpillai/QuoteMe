0469d5929865dcbf24f0f7f61d1de01094a16063
Laura Gouillon <lgouillon@yahoo.com>
2015-11-22 15:01:11 -0800

    Changed name of NotificationGUI to NotificationPageGUI

--- src/client/SearchResultsGUI.java
+++ src/client/SearchResultsGUI.java
@@ -5,6 +5,7 @@ import java.awt.GridLayout;
 import java.util.Date;
 import java.util.Vector;
 
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
@@ -18,6 +19,7 @@ public class SearchResultsGUI extends JPanel {
 	private MainPanel mp;
 	
 	private JPanel outerPanel;
+	private JScrollPane scrollPane;
 
 	public SearchResultsGUI(Vector<User> u, Vector<Quote> q, MainPanel mp) {
 		users = u;
@@ -46,16 +48,32 @@ public class SearchResultsGUI extends JPanel {
 			}
 		}
 		
+		/*
 		if (users.size() != 0) {
 			Quote q = new Quote("QUOTE quote fuck", users.elementAt(0), users.elementAt(0), new Date(0), 0);
 			QuoteGUI quote = new QuoteGUI(mp, q);
 			quoteResults.add(quote);
-		}
+		}*/
 	}
 
 	private void createGUI() {
-		int numberRows = userResults.size() + quoteResults.size();
-		setLayout(new GridLayout(numberRows, 1));
+		
+		JPanel centerPanel = new JPanel();
+		JPanel resultsPanel = new JPanel();
+		resultsPanel.setLayout(new BoxLayout(resultsPanel, BoxLayout.Y_AXIS));
+//		resultsPanel.add(new QuoteGUI(mainPanel, quote1));
+//		resultsPanel.add(new QuoteGUI(mainPanel, quote2));
+//		resultsPanel.add(new QuoteGUI(mainPanel, quote3));
+		
+		scrollPane = new JScrollPane(resultsPanel);
+		
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		centerPanel.add(scrollPane);
+		add(centerPanel, BorderLayout.CENTER);
+		
+		
+		//int numberRows = userResults.size() + quoteResults.size();
+		//setLayout(new GridLayout(numberRows, 1));
 		for (UserResultGUI u : userResults) {
 			add(u);
 		}
